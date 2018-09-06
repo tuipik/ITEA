8e8980d515edda70af5bf144a556c50ec2f56d91
tuipik <tuipiku@gmail.com>
2018-09-06 17:23:22 +0300

    added Lesson_07

---
+++ Lesson_07/test_homework_03.py
@@ -0,0 +1,33 @@
+import pytest
+
+@pytest.parametrize('a,b', [
+	('spam': bytes('4:spam')),
+	('': bytes('0:')),
+	(')(*&&^*(&#@^||', )
+])
+
+def test_encode_str(a,b):
+	pass
+
+@pytest.parametrize('a,b', [
+	(3: 'i3e'),
+	(-3: 'i-3e')
+])
+def test_encode_bytes(a,b):
+	pass
+
+@pytest.parametrize('a,b', [
+	('spam': bytes('4:spam'))
+])
+
+
+
+
+
+
+
+
+
+
+
+def
