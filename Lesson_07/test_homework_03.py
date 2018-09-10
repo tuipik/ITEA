15f34e6bca3a699e2a2d4405e4149bdd4fc35d2a
tuipik <tuipiku@gmail.com>
2018-09-10 17:50:16 +0300

    homework03

--- Lesson_07/test_homework_03.py
+++ Lesson_07/test_homework_03.py
@@ -27,7 +27,19 @@ def test_encode_bytes(a,b):
 
 
 
-
-
-
-def
+if isinstance(obj, int):
+        return b"i" + str(obj).encode() + b"e"
+    elif isinstance(obj, bytes):
+        return str(len(obj)).encode() + b":" + obj
+    elif isinstance(obj, str):
+        return encode(obj.encode("utf-8"))
+    elif isinstance(obj, list):
+        return b"l" + b"".join(map(encode, obj)) + b"e"
+    elif isinstance(obj, dict):
+        if all(isinstance(i, bytes) for i in obj.keys()):
+            items = list(obj.items())
+            items.sort()
+            return b"d" + b"".join(map(encode, it.chain(*items))) + b"e"
+        else:
+            raise ValueError("dict keys should be bytes " + str(obj.keys()))
+    raise ValueError("Allowed types: int, bytes, list, dict; not %s", type(obj))
