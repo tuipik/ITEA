20edabdcbf0401e6cb821fb929597c5236a4cba2
tuipik <tuipiku@gmail.com>
2018-09-20 13:26:19 +0300

    almost ready HW03

--- Lesson_07/homework03.py
+++ Lesson_07/homework03.py
@@ -1,23 +1,5 @@
-# bencoding
-# http://www.bittorrent.org/beps/bep_0003.html
 
-# Strings are length-prefixed base ten followed by a colon and the string.
-# For example 4:spam corresponds to 'spam'.
-
-# Integers are represented by an 'i' followed by the number in base 10 followed by an 'e'.
-# For example i3e corresponds to 3 and i-3e corresponds to -3.
-# Integers have no size limitation. i-0e is invalid.
-# All encodings with a leading zero, such as i03e, are invalid,
-# other than i0e, which of course corresponds to 0.
-
-# Lists are encoded as an 'l' followed by their elements (also bencoded) followed by an 'e'.
-# For example l4:spam4:eggse corresponds to ['spam', 'eggs'].
-
-# Dictionaries are encoded as a 'd' followed by a list of alternating keys
-# and their corresponding values followed by an 'e'.
-# For example, d3:cow3:moo4:spam4:eggse corresponds to {'cow': 'moo', 'spam': 'eggs'}
-# Keys must be strings and appear in sorted order (sorted as raw strings, not alphanumerics).
-
+import string
 from django.conf import settings
 
 if not settings.configured:
@@ -27,22 +9,16 @@ if not settings.configured:
 	)
 
 
-
 def encode(val):
 	if type(val) == int:
 		return b'i' + bytes(str(val), 'utf-8') + b'e'
+
 	elif type(val) == str:
 		return bytes(str(len(val)) + ':' + val, 'utf-8')
-	elif type(val) == dict:
-		keys = list(val.keys())
-		keys.sort()
-		end = b'd'
-		for i in keys:
-			if type(i) == str:
-				end += encode(i)
-			end += encode(val[i])
-		end += b'e'
-		return end
+
+	elif type(val) == bytes:
+		return bytes(str(len(val)), 'utf-8') + b':' + val
+
 	elif type(val) == list:
 		end = b'l'
 		for i in val:
@@ -50,22 +26,92 @@ def encode(val):
 		end += b'e'
 		return end
 
+	elif type(val) == dict:
+		keys = []
+		for k, v in val.items():
+			keys.append(k)
+			keys.append(v)
+		end = b'd'
+		for i in keys:
+			end += encode(i)
+		end += b'e'
+		return end
+
+
+
 
 def decode(val):
-	pass
 
-# print(decode('9:sdvsdg vs'))
-# print(decode('i-0653e'))
-# print(decode('l4:spami-3345e4:eggs12:papandopulosli33ei-6ei0eee'))
-# print(decode('d3:cow3:moo4:spam4:eggse'))
-# print(decode('d4:spaml1:a1:bee'))
-# print(decode('d4:spaml4:eggs5:applee3:vool4:lark5:argsee4:wool3:cow2:xo4:tezee'))
-#
-#
-#
-# print(encode('sdvsdg vs'))
-# print(encode(-3))
-# print(encode(['spam', -3345, 'eggs', 'papandopulos', [33, -6, 0]]))
-# print(encode({b'cow': 'moo', b'spam': 'eggs'}))
-# print(encode({b'spam': ['a', 'b']}))
-# print(encode({b'spam': ['eggs', 'apple'], b'voo': ['lark', 'argse'], b'wool': b'cow', b'xo': b'teze'}))
+	def decode_b_str(val):
+		if val.startswith(b"i"):
+			result = int(val[1:val.find(b'e')])
+			return result, val[val.find(b'e') + 1:]
+
+		elif str(val, 'utf-8')[0] in string.digits:
+			dig = True
+			counter = 0
+			while dig:
+				if str(val, 'utf-8')[counter] in string.digits:
+					counter += 1
+				else:
+					dig = False
+			integer = int(val[0:counter])
+			if counter > 1:
+				return val[counter+1:integer+counter+1], val[(integer+counter+1):]
+			else:
+				return val[2:(int(str(val, 'utf-8')[0]) + 2)], val[(int(str(val, 'utf-8')[0]) + 2):]
+
+		elif val.startswith(b"l"):
+			decoded_list = []
+			new_b_str = val[1:]
+			while not new_b_str.startswith(b"e"):
+				returned_str = decode_b_str(new_b_str)[0]
+				new_b_str = decode_b_str(new_b_str)[1]
+				decoded_list.append(returned_str)
+			new_b_str = new_b_str[1:]
+			return decoded_list, new_b_str
+
+		elif val.startswith(b"d"):
+			decoded_list = []
+			new_b_str = val[1:]
+			while not new_b_str.startswith(b"e"):
+				returned_str, new_b_str = decode_b_str(new_b_str)
+				decoded_list.append(returned_str)
+			new_b_str = new_b_str[1:]
+			result_dict = dict(zip(decoded_list[::2], decoded_list[1::2]))
+			return result_dict, new_b_str
+
+		else:
+			raise ValueError("Incorect value")
+
+	if isinstance(val, str):
+		val = val.encode("utf-8")
+
+	returned_data = decode_b_str(val)[0]
+	new_b_str = decode_b_str(val)[1]
+
+	if new_b_str:
+		raise ValueError("Incorect value")
+	return returned_data
+
+
+print(decode(b'9:sdvsdg vs'))
+print(decode(b'i-0653e'))
+print(decode(b'1: '))
+print(decode(b'l4:spam4:eggse'))
+print(decode(b'l4:spami3345e4:eggs53:papandopusvdvsewwwwwwwwwwwwwwwwwwwscscvsvseeeeeeeelosli33ei-6ei0eee'))
+print(decode(b'd3:cow3:moo4:spam4:eggse'))
+print(decode(b'd4:spaml1:a1:bee'))
+print(decode(b'd4:spaml4:eggs5:applee3:vool4:lark5:argsee4:wool3:cow2:xo4:tezee'))
+print(decode(encode({b'spam': [b'eggs', b'apple'], b'voo': [b'lark', b'argse'], b'wool': b'cow', b'xo': b'teze'})))
+print()
+print()
+print(encode(b'sdvsdg vs'))
+print(encode(b'sdvsvvsdvdg sdvsdvvs'))
+print(encode(-653))
+print(encode(' '))
+print(encode([b'spam', b'eggs']))
+print(encode([b'spam', 3345, b'eggs', b'papandopusvdvsewwwwwwwwwwwwwwwwwwwscscvsvseeeeeeeelos', [33, -6, 0]]))
+print(encode({b'cow': b'moo', b'spam': b'eggs'}))
+print(encode({b'spam': [b'a', b'b']}))
+print(encode({b'spam': [b'eggs', b'apple'], b'voo': [b'lark', b'argse'], b'wool': b'cow', b'xo': b'teze'}))
